6a41c5c3e25c056a2c5153750da79e5ca064cf41
stefan789 <stefan.stuiber@gmail.com>
2013-09-21 16:55:33 +0200

    first version layout

--- initLayout.py
+++ initLayout.py
@@ -22,48 +22,47 @@ class initLayout(wx.Frame):
         controlbag = wx.GridBagSizer(hgap = 5, vgap = 5)
         progressbag = wx.GridBagSizer(hgap = 5, vgap = 5)
         controlbag.Add((380,100), pos = (0,0), flag = wx.EXPAND)
-        progressbag.Add((380,100), pos = (0,0), flag = wx.EXPAND)
+        progressbag.Add((380,165), pos = (0,0), flag = wx.EXPAND)
 
         # create start button and add them to controlbag
         startbuttonSizer = wx.BoxSizer(wx.HORIZONTAL)
-        startbtn = wx.Button(self.panel, label = 'Start', size = (200,100))
-        startbuttonSizer.Add((100,100), wx.ALIGN_LEFT)
-        startbuttonSizer.Add(startbtn,flag = wx.EXPAND)
-        startbuttonSizer.Add((-1,100), wx.ALIGN_RIGHT)
+        startbtn = wx.Button(self.panel, label = 'Start', size = (200,123))
+        startbuttonSizer.Add((-1,-1), wx.ALIGN_LEFT|wx.EXPAND)
+        startbuttonSizer.Add(startbtn)
+        startbuttonSizer.Add((-1,-1), wx.ALIGN_RIGHT|wx.EXPAND)
         controlbag.Add(startbuttonSizer, pos = (1,0), flag = wx.EXPAND)
         controlbag.AddGrowableRow(1,0)
 
-        controlbag.Add((-1,50), pos = (2,0), flag = wx.EXPAND)
+        controlbag.Add((-1,40), pos = (2,0), flag = wx.EXPAND)
 
         # create abort button and add them to controlbag
         abortbuttonSizer = wx.BoxSizer(wx.HORIZONTAL)
-        abortbtn = wx.Button(self.panel, label = 'Abort', size = (200,100))
-        abortbuttonSizer.Add((100,100), wx.ALIGN_LEFT)
+        abortbtn = wx.Button(self.panel, label = 'Abort', size = (200,123))
+        abortbuttonSizer.Add((-1,-1), wx.ALIGN_LEFT|wx.EXPAND)
         abortbuttonSizer.Add(abortbtn,flag = wx.EXPAND)
-        abortbuttonSizer.Add((-1,100), wx.ALIGN_RIGHT)
+        abortbuttonSizer.Add((-1,-1), wx.ALIGN_RIGHT|wx.EXPAND)
         controlbag.Add(abortbuttonSizer, pos = (3,0), flag = wx.EXPAND)
         controlbag.AddGrowableRow(3)
 
         
         # create overall progress bar and add to progressbag
-        self.overalllabel = wx.StaticText(self.panel, wx.ID_ANY, "Overall progress", size=(-1,70), style=wx.ALIGN_BOTTOM)
+        self.overalllabel = wx.StaticText(self.panel, wx.ID_ANY, "Overall progress", size=(-1,-1), style=wx.ALIGN_BOTTOM)
         progressbag.Add(self.overalllabel, pos = (1,0), flag = wx.ALIGN_LEFT|wx.EXPAND)
         progressbag.AddGrowableRow(1)
-        self.overallbar = wx.Gauge(self.panel, range = 100, size = (380, 30))
+        self.overallbar = wx.Gauge(self.panel, range = 100, size = (-1,-1))
         self.overallbar.SetValue(20)
-        progressbag.Add(self.overallbar, pos = (2,0))
+        progressbag.Add(self.overallbar, pos = (2,0), flag = wx.EXPAND)
 
         # spacer in between progress bars
         progressbag.Add((-1,40), pos = (3,0), flag = wx.EXPAND)
-        progressbag.AddGrowableRow(3)
         # create overall progress bar and add to progressbag
         self.currentlabel = wx.StaticText(self.panel, wx.ID_ANY, "Current progress")
         progressbag.Add(self.currentlabel, pos = (4,0), flag = wx.ALIGN_LEFT)
         progressbag.AddGrowableRow(4)
-        self.currentbar = wx.Gauge(self.panel, range = 100, size = (380, 30))
+        self.currentbar = wx.Gauge(self.panel, range = 100, size = (-1,-1))
         self.currentbar.SetValue(70)
-        progressbag.Add(self.currentbar, pos = (5,0))
-        progressbag.Add((-1,-1), pos = (6,0), flag = wx.EXPAND)
+        progressbag.Add(self.currentbar, pos = (5,0), flag = wx.EXPAND)
+        progressbag.Add((-1,-1), pos = (6,0))
 
 
         # add them to their boxes
@@ -71,8 +70,8 @@ class initLayout(wx.Frame):
         progressbox.Add(progressbag, wx.EXPAND)
         
         # add boxes to outerbag
-        outerbagSizer.Add(controlbox, pos = (0,0), flag=wx.ALIGN_CENTER_VERTICAL, border = 5)
-        outerbagSizer.Add(progressbox, pos = (0,1), flag=wx.ALIGN_CENTER_VERTICAL, border = 5)
+        outerbagSizer.Add(controlbox, pos = (0,0), flag=wx.ALIGN_TOP, border = 5)
+        outerbagSizer.Add(progressbox, pos = (0,1), flag=wx.ALIGN_TOP, border = 5)
 
         # add outerbag to top
         topSizer.Add(outerbagSizer, wx.EXPAND)
