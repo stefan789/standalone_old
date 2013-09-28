e614b48a3c1f2f4ed1cf9923a6d8936600937a15
stefan789 <stefan.stuiber@gmail.com>
2013-09-28 16:17:11 +0200

    fixed demonstration timer

--- control.py
+++ control.py
@@ -7,11 +7,12 @@ class control():
         self.gui = klasse
         self.main = klasse.mainwin
 
-
+        # bind buttons in mainwindow
         self.main.Bind(wx.EVT_BUTTON, self.onStart, self.main.startbtn)
         self.main.Bind(wx.EVT_BUTTON, self.onAbort, self.main.abortbtn)
         self.main.Bind(wx.EVT_BUTTON, self.onAdv, self.main.advbtn)
 
+        # create timers for progress bars
         self.overallbar = self.main.overallbar
         self.overalltimer = wx.Timer(self.main, 999)
         self.currentbar = self.main.currentbar
@@ -19,7 +20,7 @@ class control():
 
 
     def on_overalltimer(self, e):
-        self.overallcount += self.duration/0.1
+        self.overallcount += 100*0.5/(self.duration)
         self.overallbar.SetValue(self.overallcount)
         if self.overallcount >= 100:
             self.overallcount = 0
@@ -29,15 +30,25 @@ class control():
             dia.ShowModal()
 
     def on_currenttimer(self, e):
-        self.count += self.coilduration/0.1
+        self.count += (100*0.1)/self.coilduration
         self.currentbar.SetValue(self.count)
         if self.count >= 100:
-            self.currenttimer.Stop()
-            self.currentbar.SetValue(0)
-            self.count = 0
+            self.coilcounter += 1
+            if self.coilcounter == self.nrCoils:
+                self.currenttimer.Stop()
+                self.currentbar.SetValue(0)
+                self.count = 0
+            else:
+                self.currenttimer.Stop()
+                self.currentbar.SetValue(0)
+                self.count = 0
+                self.currenttimer.Start(100)
 
 
     def onAbort(self, e):
+        """
+        dialog to abort degaussing
+        """
         dlg = wx.MessageDialog(self.main.panel, "Do you really want to quit the app?", "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
         result = dlg.ShowModal()
         dlg.Destroy()
@@ -45,25 +56,37 @@ class control():
             self.main.Destroy()
 
     def onStart(self, e):
+        """
+        begins degaussing
+        """
         # dauer hier berechnet, nachher aus parametern von dodegauss
         self.nrCoils = 3
-        self.coilduration = 3
+        self.coilduration = 5
         self.duration = self.nrCoils * self.coilduration
         self.count = 0
         self.overallcount = 0
+        self.coilcounter = 0
+
+        # bind timers and start
         self.main.Bind(wx.EVT_TIMER, self.on_overalltimer, self.overalltimer,
                 999)
         self.main.Bind(wx.EVT_TIMER, self.on_currenttimer, self.currenttimer,
                 1000)
         self.currenttimer.Start(100)
-        self.overalltimer.Start(100)
+        self.overalltimer.Start(500)
 
     def onAdv(self, e):
+        """ 
+        creates advanced settings window
+        """
         self.gui.createAdvWindow()
         self.adv = self.gui.advwin
         self.main.Bind(wx.EVT_BUTTON, self.onAdvOk, self.adv.okbutton)
         self.adv.Show()
 
     def onAdvOk(self, e):
+        """
+        called on click on ok button in advanced settings window
+        """
         self.adv.Destroy()
 
